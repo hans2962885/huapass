42520c99e3edfd5031a2bfc04b9e61ece5b304f8
Hans <hann91821@gmail.com>
2026-07-26 16:34:47 +0700

    Web sound fixes: voice-list wait + iOS Safari speech unlock

--- app/flutter_bootstrap.js
+++ app/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"a10d8ac38de835021c8d2f920dbf50a920ccc0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2477826949" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "599292740" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
